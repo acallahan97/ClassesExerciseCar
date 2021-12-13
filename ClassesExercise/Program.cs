ead5dcfb2c42e7b9654757ae2ec1b1a1ee89cdd9
acallahan97 <callahanamanda877@gmail.com>
2021-12-13 12:36:54 -0700

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car printCar = new Car() { Make = "Honda", Model = "CRV", Year = 2010 };
+            Console.WriteLine($"Make: {printCar.Make}, Model: {printCar.Model}, Year: {printCar.Year}");
+        }
+    }
+}
